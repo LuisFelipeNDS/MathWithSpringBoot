a56b5cf94069b35000a47538e66549696f238074
Felipe <Felipesss.2014@gmail.com>
2023-11-20 16:15:04 -0300

    Metodo sum, isNumeric e convertToDouble implementados

--- src/main/java/com/Math/Controller/MathController.java
+++ src/main/java/com/Math/Controller/MathController.java
@@ -23,12 +23,26 @@ public class MathController {
     }
 
 	private Double convertToDouble(String strNumber) {
+		if(strNumber == null) {
+			return 0d;
+		}
 		
+		String number = strNumber.replaceAll( ",", ".");
+		
+		if(isNumeric(number)) {
+			return Double.parseDouble(number);
+		}
+			
 		return null;
 	}
 
-	private boolean isNumeric(String numberOne) {
-
-		return false;
+	private boolean isNumeric(String strNumber) {
+		
+		if(strNumber == null) {
+			return false;
+		}
+		
+		String number = strNumber.replaceAll( ",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 }
